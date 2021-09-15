1339366eff1c72b3c56cf2b2679b3b12eb757732
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2021-09-14 20:12:37 -0500

    Update MarsImageConstants.cs

--- src/WebSpa/Constants/MarsImageConstants.cs
+++ src/WebSpa/Constants/MarsImageConstants.cs
@@ -5,6 +5,6 @@
         public const string Image_Name_Prefix = "mars_image_";
         public const string Image_Folder_Name = @"wwwroot\";
         public const string Dates_File_Name = @"Dates\dates.txt";
-        public const string NASA_API_Key = "MduMgCjq0GacFwpi1oYxIZbMq0ubZnHzcw7fcBV5";
+        public const string NASA_API_Key = "";  //Please_Add_API_Key
     }
 }
